7b5752a9388b60395794b6a81ea900c3cf1d3406
Nasir-Jamal <39132449+Nasir-Jamal@users.noreply.github.com>
2021-04-15 01:23:40 +0800

    Modification in code

---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Hello()
+{
+    return <p>This is para in Hello Tag!</p>
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -6,10 +6,32 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+    <span><div className="div_name">Hello <strong>Nasir Jamal</strong></div></span>,
+    <span className="span_">Welcome to React App Development in Bootcamp 2020</span>,
+    <div className="div_App"><App name="Nasir Jamal" age={38}/></div>,
+    <div className="div_ol">
+      <ol>
+        <li>CSS</li>
+        <li>HTM5</li>
+        <li>JavaScript</li>
+        </ol></div>,
+     <div className="div_ul">
+       <ul>
+         <li>NodeJS</li>
+         <li>ReactJS</li>
+         <li>Vue.JS</li>
+         </ul></div>,    
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+/*
+ReactDOM.render(
+  React.createElement('div', null, "Hello World with cretaeElement"),
+  document.getElementById('root'),
+)
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
